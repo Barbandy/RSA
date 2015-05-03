eab3588a32041f15009dae465683f7820c22cf6a
barbandy <maximsosnovskiy@gmail.com>
2015-05-03 13:19:25 +0700

    v2.1

--- withBigInt/rsa.py
+++ withBigInt/rsa.py
@@ -7,7 +7,16 @@ import BigInt
 def getArgs():
     parser = argparse.ArgumentParser()
     parser.add_argument('inFile')
+    parser.add_argument('outFile')
+    parser.add_argument('mode', choices=['e', 'd'])
     return parser.parse_args()
+
+
+def GeneratePrime(bitLen):
+    p = BigInt.GenerateRandomLen(bitLen)
+    while not MillerRabin(p):
+        p += 1
+    return p	
 
 	
 # проверка простого числа	
@@ -57,70 +66,70 @@ def MillerRabin(m):
     m = m.getString()
     m = int(m)
     t = m - 1
-    s = 0
+    s = BigInt.BigInt(0)
     while t % 2 == 0:
         t /= 2
         s += 1
-            
-    for i in range(20):
-        #a = BigInt.GenerateRandomMax(m - 1)
+       
+    for step in range(20):
+        #a = BigInt.GenerateRandomMax(m-1)
         a = random.randint(1, m-1)
         x = pow(a, t, m)
         if x == 1:
             return True # составное
-        i = BigInt.BigInt(0)
-    for i in range(s - 1):
-        x = pow(x, 2, m)
+    i = BigInt.BigInt(0)
+    while i < s - 1:
+        x = (x * x) % m
         if x == m - 1:
             return True
+        i = i + 1
     return x == m - 1
 				
 		
 # генерация ключей		
-def KeyGen(p, q, e):	
+def KeyGen(bitlen):
+    p = BigInt.BigInt()
+    q = BigInt.BigInt()
+    e = BigInt.BigInt()
+    p = GeneratePrime(bitlen)
+    q = GeneratePrime(bitlen)
+    while p == q:
+        q = GeneratePrime(bitlen)
+    e = GeneratePrime(40)	
     n = p * q
     fi = (p - BigInt.BigInt(1)) * (q - BigInt.BigInt(1))
     d = GCD_ex(e, fi)
     pub_key = "{}\n{}".format(e, n)
-    print "pub_key = ", pub_key
     priv_key = "{}\n{}".format(d, n)
-    print "priv_key = ", priv_key
-
-    return n, d
+  
+    return pub_key, priv_key, n, d, e
 
 		
 def main():
     print "RSA"
     args = getArgs()
-	
+    bitlen = 1024	
     msg = BigInt.BigInt()
     msg.getFrom_txt(args.inFile)
-	
-    p = BigInt.BigInt()
-    q = BigInt.BigInt()
-    e = BigInt.BigInt()
-	
-    p.getFrom_txt("p.txt")
-    q.getFrom_txt("q.txt")
-    e.getFrom_txt("e.txt")
+    if 	args.mode == "e":
+        pub_key, priv_key, n, d, e = KeyGen(bitlen)
+        msg = BigInt.BigInt(str(msg))
+        c =  powmod(msg, e, n)
+        c.saveTo_txt(args.outFile)
+        with open('pub.key', 'w') as pub:
+            pub.write(pub_key)
+        with open('priv.key', 'w') as priv:
+            priv.write(priv_key)
 
-    Test_Prime(p)
-    Test_Prime(q)
-    Test_Prime(e)
-	
-    n, d = KeyGen(p, q, e)
-    print "text = ", msg
-    msg = BigInt.BigInt(str(msg))
-    c =  powmod(msg, e, n)
-    print "encrypt_text = ", c
-    text = powmod(c, d, n)
+    if args.mode == "d":   
+        c = BigInt.BigInt()
+        with open('priv.key') as priv:
+            (d, n) = priv.read().split("\n") 
+        d = BigInt.BigInt(d)
+        n = BigInt.BigInt(n)     
+        c.getFrom_txt(args.outFile)
+        text = powmod(c, d, n)
+        text.saveTo_txt("res.txt")
 		
-    print "decrypt_text = ", text
-    
-    if msg == text:
-        print"Success!"
-    else:
-        print "Failure!"
-
 if __name__ == "__main__":
     main()	
--- withBigInt/test_rsa.py
+++ withBigInt/test_rsa.py
@@ -2,29 +2,13 @@
 import random, pytest, rsa, BigInt
 
 
-def GeneratePrime(bitLen):
-    p = BigInt.GenerateRandomLen(bitLen)
-    while not rsa.MillerRabin(p):
-        p += 1
-    return p
-
-
 def test_rsa():
     msg = BigInt.BigInt()
-    p = BigInt.BigInt()
-    q = BigInt.BigInt()
-    e = BigInt.BigInt()
-    p = GeneratePrime(1024)
-    q = GeneratePrime(1024)
-    e = GeneratePrime(40)
-    n, d = rsa.KeyGen(p, q, e)
+    n = BigInt.BigInt()
+    d = BigInt.BigInt()
+    pub, priv, n, d, e = rsa.KeyGen(1024)
     msg = BigInt.GenerateRandomMax(n-1)
     msg = BigInt.BigInt(str(msg))
     c =  rsa.powmod(msg, e, n)
     text = rsa.powmod(c, d, n)
     assert msg == text
-
-	
-
-
-
